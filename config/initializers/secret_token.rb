93c51661801372fd06ebe5287275bb8e51ab97e2
gabrieltong <tongyangmath@gmail.com>
2013-12-25 10:51:27 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubManager::Application.config.secret_token = '9bd1850dc96e4d11cb31eb736f3f5dafab6d4d08a8dba63a31db2e3819eefb1c69f2397bd6a1f099150028cd3f53214c3c1afadf8ca775a53c91f17377dc49f2'
