4fc7213a77d42bb2ab41a816f79c83f14d62435d
gabrieltong <tongyangmath@gmail.com>
2013-12-25 16:20:18 +0800

    成功导入starrings

--- app/models/gb_user.rb
+++ app/models/gb_user.rb
@@ -3,18 +3,22 @@ class GbUser < ActiveRecord::Base
   attr_accessible :username
 
   has_and_belongs_to_many :gb_repos
-    
-  def import_starrings
+
+  def import_starrings(page=1,per_page=100)
     require 'json'
 
-    url,options  = 'https://api.github.com/users/gabrieltong/starred',{'User-Agent'=>'gabrieltong'}
+    url,options  = "https://api.github.com/users/gabrieltong/starred?per_page=#{per_page}&page=#{page}",{'User-Agent'=>'gabrieltong'}
 
     response = HTTParty.get(url,:headers =>options)
-    # p response.body
-    # p response.code
-    # p response.message
     
-    # p response.headers.inspect
-    p JSON.parse response.body
+    items = JSON.parse(response.body)
+    items.each do |item|
+      repo = GbRepo.find_or_initialize_by_address item['html_url']
+      repo.extra = item
+      p repo.valid?
+      p repo.errors if !repo.valid?
+      gb_repos << repo
+    end
+    import_starrings(page+1) unless items.blank?
   end
 end
